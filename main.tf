f4b58573a19d0b64b0ed935e04ddcb090223a0a0
JenJen123123 <ssystem32@gmail.com>
2025-06-17 21:04:19 +0300

    Refactor Terraform configuration: replace hardcoded values with data lookups for VPC, security group, and subnets; update output variables; comment out unused variable definitions.

--- main.tf
+++ main.tf
@@ -1,20 +1,93 @@
+############################################
+# Look-ups for resources you created manually
+############################################
+
+# 1) Lab VPC
+data "aws_vpc" "lab" {
+  filter {
+    name   = "tag:Name"
+    values = ["Lab VPC"]
+  }
+}
+
+# 2) Web Security Group
+data "aws_security_group" "web" {
+  filter {
+    name   = "tag:Name"          # native attribute
+    values = ["Web Security Group"]  #  ← must match exactly
+  }
+}
+
+# 3) Private Subnet 1
+data "aws_subnet" "private1" {
+  filter {
+    name   = "tag:Name"
+    values = ["Private Subnet 1"]
+  }
+}
+
+# 4) Private Subnet 2
+data "aws_subnet" "private2" {
+  filter {
+    name   = "tag:Name"
+    values = ["Private Subnet 2"]
+  }
+}
+
+# 5) Existing EC2 Instance
+data "aws_instances" "by_name" {
+  filter {
+    name   = "tag:Name"
+    values = ["Web Server 1"]
+  }
+}
+
+# Read the first matching ID
+data "aws_instance" "existing" {
+  instance_id = one(data.aws_instances.by_name.ids)
+}
+
+
+############################################
+# Existing modules now fed by the look-ups
+############################################
+
 module "db_security_group" {
   source    = "./modules/security_group"
-  vpc_id    = var.lab_vpc_id
-  web_sg_id = var.web_sg_id
+  vpc_id    = data.aws_vpc.lab.id
+  web_sg_id = data.aws_security_group.web.id
 }
+
 module "db_subnet_group" {
-  source             = "./modules/db_subnet_group"
-  private_subnet_ids = var.private_subnet_ids
+  source = "./modules/db_subnet_group"
+  private_subnet_ids = [
+    data.aws_subnet.private1.id,
+    data.aws_subnet.private2.id
+  ]
 }
+
 module "rds" {
   source = "./modules/rds"
 
-  db_subnet_group_name    = module.db_subnet_group.subnet_group_name
-  db_username             = var.db_username
-  db_password             = var.db_password
-  vpc_security_group_ids  = [module.db_security_group.db_security_group_id]
+  db_subnet_group_name   = module.db_subnet_group.subnet_group_name
+  db_username            = var.db_username
+  db_password            = var.db_password
+  vpc_security_group_ids = [module.db_security_group.db_security_group_id]
 }
-output "notice" {
-  value = "Endpoint: ${module.rds.rds_endpoint}"
+
+output "Endpoint" {
+  value = module.rds.rds_endpoint
+}
+output "DB_name" {
+  value = "jen_db"
+}
+output "User_name" {
+  value = "Jen"
+}
+output "Password" {
+  value = "12341234"
+}
+output "Public_ip" {
+  description = "Public IP of the manually-created EC2 instance"
+  value       = data.aws_instance.existing.public_ip
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
-lab_vpc_id = "vpc-0cc1bd07d220b6b4f"
-web_sg_id = "sg-0c1be7c37b9ecfc17"
-private_subnet_ids = ["subnet-04bf59f51ed063eef", "subnet-07ca64f709ca5be00"]
+#lab_vpc_id = "vpc-0cc1bd07d220b6b4f"
+#web_sg_id = "sg-0c1be7c37b9ecfc17"
+#private_subnet_ids = ["subnet-04bf59f51ed063eef", "subnet-07ca64f709ca5be00"]
 db_username = "jen"
 db_password = "12341234"
